23e17da1d1b159c87302b88ad7036da685c5452f
Carlos Eduardo <carlosouzavalle@gmail.com>
2021-01-28 20:54:20 -0300

    script de movimentacao de personagem

---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float vel;
+    private Rigidbody rig;
+    
+
+    public float[] positions;
+
+    private bool isMoving = false;
+    private Vector3 destination;
+
+    // responsavel por guardar o estado atual
+    private PositionState currentState;
+
+    // lista de inteiros
+    public enum PositionState
+    {
+        left,
+        middle,
+        right,
+        
+    }
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rig = GetComponent<Rigidbody>();
+        currentState = PositionState.middle;
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.A) && !isMoving)
+        {
+            // transform.Translate(Vector3.left * Time.deltaTime * vel);
+            switch (currentState)
+            {
+                case PositionState.left:
+                    break;
+                case PositionState.right:
+                    currentState = PositionState.middle;
+                    
+                    // cetar a posição do meio
+                    break;
+                case PositionState.middle:
+                    currentState = PositionState.left;
+                    // posição para a esquerda
+                    break;
+                default:
+                    break;
+            }
+            //transform.position = new Vector3(positions[(int)currentState], 0, 0);
+            MovetoLine(new Vector3(positions[(int)currentState], 0, 0));
+        }
+
+        if(Input.GetKey(KeyCode.D) && !isMoving)
+        {
+            // transform.Translate(Vector3.right * Time.deltaTime * vel);
+            switch (currentState)
+            {
+                case PositionState.left:
+                    currentState = PositionState.middle;
+                    break;
+                case PositionState.right:
+                    break;
+                case PositionState.middle:
+                    currentState = PositionState.right;
+                    break;
+                default:
+                    break;
+            }
+            //transform.position = new Vector3(positions[(int)currentState], 0, 0);
+            MovetoLine(new Vector3(positions[(int)currentState], 0, 0));
+        }  
+        
+        if(isMoving)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, destination, Time.deltaTime * vel);
+            // distance = esse metodo dis qual a distancia entre dois pontos
+            if (Vector3.Distance(transform.position, destination) < 0.01f)
+            {
+                isMoving = false;
+            }
+        }
+    }
+
+    void MovetoLine(Vector3 dest)
+    {
+        destination = dest;
+        isMoving = true;
+    }
+}
+
+
+
---
+++ Assets/scripts/chao.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class chao : MonoBehaviour
+{
+
+    public Vector3 vel;
+    public float altura;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(vel * Time.deltaTime);
+        if (transform.position.z < -9.0f)
+        {
+            float repos = 45.0f - (-9.0f - transform.position.z);
+            transform.position = new Vector3(0.0f, altura, repos);
+        }
+    }
+}
