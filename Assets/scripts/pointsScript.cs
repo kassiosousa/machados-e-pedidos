e9e977cd1214356aaf77e0c09a202d12af095a1d
Omar Mohamad Abou Ghoche <omarghoucher@gmail.com>
2021-01-29 13:44:04 -0300

    criacao do controle de estrelas

---
+++ Assets/scripts/pointsScript.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pointsScript : MonoBehaviour
+{
+
+    public GameObject[] estrelas = new GameObject[5];
+    public int counter;
+
+    void Start()
+    {
+        PlayerPrefs.SetInt("estrelas", 5);
+        counter = PlayerPrefs.GetInt("estrelas");
+    }
+
+    void Update()
+    {
+        //AddEstrela();
+        //SubEstrela();
+    }
+
+
+    public void AddEstrela()
+    {
+        if (counter < 5)
+        {
+            switch (counter)
+            {
+                case 0:
+                    estrelas[0].SetActive(true);
+                    break;
+                case 1:
+                    estrelas[1].SetActive(true);
+                    break;
+                case 2:
+                    estrelas[2].SetActive(true);
+                    break;
+                case 3:
+                    estrelas[3].SetActive(true);
+                    break;
+                case 4:
+                    estrelas[4].SetActive(true);
+                    break;
+                default:
+                    break;
+            }
+
+            counter++;
+            PlayerPrefs.SetInt("estrelas", counter);
+
+        }
+    }
+
+
+    public void SubEstrela()
+    {
+        if (counter > 0)
+        {
+            switch (counter)
+            {
+                case 1:
+                    estrelas[0].SetActive(false);
+                    // GameOver
+                    break;
+                case 2:
+                    estrelas[1].SetActive(false);
+                    break;
+                case 3:
+                    estrelas[2].SetActive(false);
+                    break;
+                case 4:
+                    estrelas[3].SetActive(false);
+                    break;
+                case 5:
+                    estrelas[4].SetActive(false);
+                    break;
+                default:
+                    break;
+            }
+            counter--;
+            PlayerPrefs.SetInt("estrelas", counter);
+            
+        }
+    }
+
+}
