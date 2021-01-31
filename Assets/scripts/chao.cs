f8b844c2bd0a788f5f7e7cd8ac32b5cc64c412ee
Kassio Sousa <kassiomaximus10@gmail.com>
2021-01-31 15:23:13 -0300

    adjustment on street velocity

--- Assets/scripts/chao.cs
+++ Assets/scripts/chao.cs
@@ -11,20 +11,26 @@ public class Chao : MonoBehaviour
     // a porta vai usar ele pra se resetar
     public UnityEvent OnReset;
     // Start is called before the first frame update
+
+
+    private GameManagerScript gameManager;
     void Start()
     {
-
+        gameManager = GameManagerScript.instance;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(vel * Time.deltaTime);
-        if (transform.position.z < limitReposition)
+        if (!gameManager.playerController.IsDead)
         {
-            float repos = 250.0f - (limitReposition - transform.position.z);
-            transform.position = new Vector3(0.0f, altura, repos);
-            OnReset.Invoke();
+            transform.Translate(vel * Time.deltaTime);
+            if (transform.position.z < limitReposition)
+            {
+                float repos = 250.0f - (limitReposition - transform.position.z);
+                transform.position = new Vector3(0.0f, altura, repos);
+                OnReset.Invoke();
+            }
         }
     }
 }
